f651721af13b288867c6846bcaa86bb04b453241
GurleenSamrow <gurleenkaur58881@gmail.com>
2023-10-04 13:09:10 +0530

    ctrl u

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import Routes from "./Routes";
 import { useEffect } from 'react';
 
 function App() {
-  // useEffect(() => {
-  //   document.addEventListener('contextmenu', (e) => {
-  //     e.preventDefault();
-  //   });
-  //   document.addEventListener('keydown', (e) => {
-  //     if (e.ctrlKey && e.key === 'u') {
-  //       e.preventDefault();
-  //     }
-  //   });
-  //   return () => {
-  //     document.removeEventListener('contextmenu', () => {});
-  //     document.removeEventListener('keydown', () => {});
-  //   };
-  // }, []);
+  useEffect(() => {
+    document.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+    });
+    document.addEventListener('keydown', (e) => {
+      if (e.ctrlKey && e.key === 'u') {
+        e.preventDefault();
+      }
+    });
+    return () => {
+      document.removeEventListener('contextmenu', () => {});
+      document.removeEventListener('keydown', () => {});
+    };
+  }, []);
   return (
 <>
 <Routes></Routes>
